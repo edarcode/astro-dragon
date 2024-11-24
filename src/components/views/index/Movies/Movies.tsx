678d63561ea1caa5cdcab1ab3241edf6533a6246
Edwin Ortiz <edarcode@gmail.com>
2024-11-24 14:28:42 -0500

    milagrosamente funciona xd

--- src/components/views/index/Movies/Movies.tsx
+++ src/components/views/index/Movies/Movies.tsx
@@ -3,15 +3,25 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import MovieCards from "./MovieCards";
 import { useState } from "react";
 import MovieDetail from "./MovieDetail";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false, // default: true,
       staleTime: 24 * 60 * 60 * 1000, // 24h
-      gcTime: 24 * 60 * 60 * 1000,
     },
   },
+});
+
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
 });
 
 export default function Movies() {
