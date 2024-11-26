d2c56889ff239a930f07810acd57eb6af9db67de
Edwin Ortiz <edarcode@gmail.com>
2024-11-26 11:46:28 -0500

    index funcional

---
+++ src/components/views/index/WhyDragon/whyDragon.ts
@@ -0,0 +1,18 @@
+export const WHY_DRAGON = [
+  {
+    id: crypto.randomUUID(),
+
+    reason:
+      "Colección de películas y series cuidadosamente seleccionadas de todos los géneros. Desde los éxitos de taquilla más recientes hasta joyas clásicas.",
+  },
+  {
+    id: crypto.randomUUID(),
+    reason:
+      "Alta calidad con recomendaciones personalizadas, tráileres y reseñas detalladas, para que siempre tengas una experiencia de entretenimiento a la altura.",
+  },
+  {
+    id: crypto.randomUUID(),
+    reason:
+      "Interfaz intuitiva y un diseño ágil, Dragon hace que explorar, descubrir y ver películas sea rápido y sencillo ¡Encuentra tu próxima historia épica en cuestión de segundos!",
+  },
+];
