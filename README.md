5550769c311c2f503bf955a0fd40e588bf548410
Edwin Ortiz <edarcode@gmail.com>
2024-11-26 14:01:30 -0500

    refactor

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ https://api.themoviedb.org/3/genre/movie/list?api_key=API_KEY&language=es-ES
 
 https://api.themoviedb.org/3/discover/movie?api_key=API_KEY&language=es-ES&with_genres=28,12&vote_average.gte=7&primary_release_year=2020&sort_by=vote_average.desc
 
-- Consultar detalle de pélicula con id 912649
+- Consultar detalle de pélicula por id 912649
 
 https://api.themoviedb.org/3/movie/912649?api_key=API_KEY&language=es-ES&append_to_response=videos
 
--- src/consts/tmdb.ts
+++ src/consts/tmdb.ts
@@ -4,6 +4,8 @@ const API_KEY = import.meta.env.PUBLIC_API_KEY;
 export const TMDB = {
   popularMovies: `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`,
   searchMoviesByTitle: `${BASE_URL}/search/movie?api_key=${API_KEY}&language=es-ES&page=1`,
+  getUrlMovieById: (id: number) =>
+    `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=es-ES&append_to_response=videos`,
   completePath: (path: string, w = 500) =>
     `https://image.tmdb.org/t/p/w${w}/${path}`,
 };
