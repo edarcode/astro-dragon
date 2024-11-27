f2ba74d231bc2d522cb7fdeac45453115e254011
Edwin Ortiz <edarcode@gmail.com>
2024-11-27 03:38:57 -0500

    agg algo de seo

---
+++ src/layouts/Layout/seo.ts
@@ -0,0 +1,7 @@
+export const SEO = {
+  title: "Buscador de películas 🎞️",
+  description:
+    "Explora nuestra colección de casi 1 millón de películas, clásicos, últimos estrenos y mucho más.",
+  author: "edarcode",
+  img: "/bladeRunner.avif",
+};
---
+++ src/layouts/Layout2/seo.ts
@@ -0,0 +1,6 @@
+export const SEO = {
+  title: "Dragon 🎞️",
+  description: "Tu próximo maratón de películas empieza aquí 🎥",
+  author: "edarcode",
+  img: "/dragon.avif",
+};
