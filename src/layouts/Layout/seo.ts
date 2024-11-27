b2772ecc8e3c1a2e8bcc624107312b561b3b6c4c
Edwin Ortiz <edarcode@gmail.com>
2024-11-27 11:19:39 -0500

    cambiando imgs de seo a jpg

--- src/layouts/Layout/seo.ts
+++ src/layouts/Layout/seo.ts
@@ -3,5 +3,5 @@ export const SEO = {
   description:
     "Explora nuestra colección de casi 1 millón de películas, clásicos, últimos estrenos y mucho más.",
   author: "edarcode",
-  img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1732701256/dragon/zy3qrzr66rhvnrooczyc.avif",
+  img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1732724279/dragon/clm3a8umsquou9hyc5p3.jpg",
 };
--- src/layouts/Layout2/seo.ts
+++ src/layouts/Layout2/seo.ts
@@ -2,5 +2,5 @@ export const SEO = {
   title: "Dragon",
   description: "Tu próximo maratón de películas empieza aquí 🎞️",
   author: "edarcode",
-  img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1732701255/dragon/qiemfpz8z5ews4ks4ibl.avif",
+  img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1732724279/dragon/nl6m2k2jn6m1jaqbnkgz.jpg",
 };
