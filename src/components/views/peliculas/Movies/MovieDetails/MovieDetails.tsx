79b5a9a5612831e631b655bc33416a74a59ad01e
Edwin Ortiz <edarcode@gmail.com>
2024-11-27 00:25:51 -0500

    agg videoYoutube trailer

--- src/components/views/peliculas/Movies/MovieDetails/MovieDetails.tsx
+++ src/components/views/peliculas/Movies/MovieDetails/MovieDetails.tsx
@@ -3,9 +3,12 @@ import { useMovies } from "../useMovies.store";
 import css from "./MovieDetails.module.css";
 import { getMovieById } from "./getMovieById.service";
 import Spinner from "src/react/components/Spinner/Spinner";
-import { TMDB } from "src/consts/tmdb";
 import Btn from "src/react/components/Btn/Btn";
 import movieDetails from "./movieDetails.avif";
+import "@justinribeiro/lite-youtube";
+import type { LiteYTEmbed } from "@justinribeiro/lite-youtube";
+import type { DOMAttributes } from "react";
+import { TMDB } from "src/consts/tmdb";
 
 export default function MovieDetails() {
   const closeDetails = useMovies((movies) => movies.closeDetails);
@@ -20,27 +23,36 @@ export default function MovieDetails() {
   if (isLoading) return <Spinner />;
   if (!movie) return null;
 
-  console.log(movie);
+  const trailers = movie.videos?.results;
+  const trailerId = trailers[trailers.length - 1]?.key;
+
   return (
     <div className={css.details}>
       <div className={css.card}>
-        <picture className={css.banner}>
-          <img
-            src={TMDB.completeImgUrl(movie.backdrop_path)}
-            alt={movie.title}
-            width={492}
-            height={277}
-          />
-        </picture>
+        {trailerId ? (
+          <div className={css.banner}>
+            <lite-youtube videoId={trailerId}>
+              <a href={`https://www.youtube.com/watch?v=${trailerId}`}>
+                Watch on YouTube: "Sample output of devtools-to-video cli tool"
+              </a>
+            </lite-youtube>
+          </div>
+        ) : (
+          <picture className={css.banner}>
+            <img
+              src={TMDB.completeImgUrl(movie.backdrop_path)}
+              alt={movie.title}
+            />
+          </picture>
+        )}
+
         <div className={css.content}>
           <h1>
             <span>{movie.title}</span>
             <span>{movie.tagline ? movie.tagline : "---"}</span>
           </h1>
-
           <p className={css.overview}>{movie.overview}</p>
           <Btn onClick={closeDetails}>Seguir buscando</Btn>
-          <Btn className={css.trailer}>Trailer</Btn>
         </div>
 
         <span className={css.average}>{movie.vote_average.toFixed()} ⭐</span>
@@ -49,4 +61,13 @@ export default function MovieDetails() {
       <img className={css.bgi} src={movieDetails.src} alt={"fondo"} />
     </div>
   );
+}
+
+type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }>;
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      ["lite-youtube"]: CustomElement<LiteYTEmbed>;
+    }
+  }
 }
